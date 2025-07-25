a8fec345e8c4a25e633a4b40577e4db135bd594a
Blankeos <carloantonioct@gmail.com>
2025-07-26 05:14:39 +0800

    feat: wasm build.

--- rust-wasm/pkg/rust_wasm.js
+++ rust-wasm/pkg/rust_wasm.js
@@ -442,9 +442,6 @@ function __wbg_get_imports() {
         const ret = arg0.getTimezoneOffset();
         return ret;
     };
-    imports.wbg.__wbg_log_29e54da1278f01d0 = function(arg0, arg1) {
-        console.log(getStringFromWasm0(arg0, arg1));
-    };
     imports.wbg.__wbg_new0_f788a2397c7ca929 = function() {
         const ret = new Date();
         return ret;
